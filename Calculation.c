a35fe82817c6b412a1eddfd4e8068158dbbc4abb
Alex Czarnick <apz2016@outlook.com>
2019-11-13 10:43:10 -0600

    Update Calculation.c

--- Calculation.c
+++ Calculation.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int calculate(int number1, int number2) {
+int function(int number1, int number2) {
     int result = number1;
     for(int i = 2; i <= number2; i++){
         result = result * number1;
@@ -9,7 +9,7 @@ int calculate(int number1, int number2) {
 }
 
 int main(){
-    int result = calculate(2, 3);
+    int result = function(2, 3);
     printf("%d\n", result);
     return 0;
 }
