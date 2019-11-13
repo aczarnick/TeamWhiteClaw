6aa1da7f3fe92135fb8efd61a694ca68c20c89d7
jacob orellana <jacoborellana97@gmail.com>
2019-11-13 10:50:00 -0600

    change code snippet file names

---
+++ snippet4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int *doSomething(int *list, int size)
+{
+    for (int i = 0, j = size - 1; i < j; i++, j--)
+    {
+        int temp = list[j];
+        list[i] = temp;
+    }
+    return list;
+}
+
+void print(int *const list, int size)
+{
+    for (int i = 0; i < size; i++)
+        printf("%d ", list[i]);
+}
+
+int main()
+{
+    int list[] = {1, 2, 3, 4, 5, 6};
+    int *p = doSomething(list, 6);
+    print(p, 6);
+    return 0;
+}
